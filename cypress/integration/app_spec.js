3111496f77cebe82fccf0a30bc3398927aeb5659
Henric Trotzig <henric.trotzig@gmail.com>
2020-05-13 12:11:24 +0200

    Flesh out cypress test
    This will make the test
    1. load the app (visit /)
    2. click the start button
    3. press space enough times for game over
    
    I'm going to add happo screenshots in between all these in a moment.

--- cypress/integration/app_spec.js
+++ cypress/integration/app_spec.js
@@ -1,6 +1,15 @@
 describe('application', () => {
-  it('successfully loads', () => {
+  it('can be successfully run', () => {
     cy.visit('/');
-    cy.get('[data-reactroot]').should('contain', 'TETRIS');
+    cy.contains('TETRIS').should('be.visible');
+
+    cy.contains('(P)').click();
+
+    cy.contains('TETRIS').should('not.be.visible');
+
+    for (let i = 0; i < 12; i++) {
+      cy.contains('(SPACE)').click();
+    }
+    cy.contains('TETRIS').should('be.visible');
   });
 });
